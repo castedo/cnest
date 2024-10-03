3aefd80cb29c110c362ef16ac10a12a4d937ad3b
Castedo Ellerman <castedo@castedo.com>
2024-10-03 12:09:09 -0400

    use --init instead of --pid=host; optional USER
    * also candidate working smoke test

--- test/opt/inner-test-nonroot.sh
+++ test/opt/inner-test-nonroot.sh
@@ -1,10 +1,8 @@
-set -o errexit
-
 INNER_CONTAINER="nested"
 set -x
 
 mkdir -p ~/Downloads
-create-nest || true # ignore https://github.com/castedo/cnest/issues/10
-create-nest only-downloads debian $INNER_CONTAINER
+
+create-cnest debian --volume $HOME/Downloads:$HOME/Downloads --name $INNER_CONTAINER
+
 cnest $INNER_CONTAINER echo PASS
-podman stop $INNER_CONTAINER
